247aec684c0920edf100fb3aab75f498b5f7638d
Eetu Sutinen <eetu.sutinen@gmail.com>
2020-01-29 13:34:26 +0200

    Add graphile

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import * as express from 'express';
+import postgraphile from 'postgraphile';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(
+  postgraphile(process.env.DATABASE_URL, 'public', {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+  }),
+);
+
+app.listen(process.env.PORT);
+
+process.stdout.write(`Server listening on port ${process.env.PORT}\n`);
